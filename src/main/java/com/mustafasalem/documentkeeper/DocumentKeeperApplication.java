e80d8b678fca644371eda4125897837a10bea73d
Salem <49875736+mustafa-salem@users.noreply.github.com>
2024-09-17 14:26:55 +0200

    chore: initial commit

---
+++ src/main/java/com/mustafasalem/documentkeeper/DocumentKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.mustafasalem.documentkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafasalem/documentkeeper/DocumentKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafasalem.documentkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
